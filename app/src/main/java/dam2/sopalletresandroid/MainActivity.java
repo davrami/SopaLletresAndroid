3326939f34a1ee8e46b6fe88558745880c41bc8c
davrami <david.rami007@gmail.com>
2017-11-27 19:38:31 +0100

    version gridview

--- app/src/main/java/dam2/sopalletresandroid/MainActivity.java
+++ app/src/main/java/dam2/sopalletresandroid/MainActivity.java
@@ -1,13 +1,79 @@
 package dam2.sopalletresandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    protected static final String EXTRA_MISSATGE = "dam2.sopadelletresandroid";
+    protected Button buttonGoPrincipal; //Declare button in view
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonGoPrincipal = (Button) findViewById(R.id.btPrincipal); //Link button in view with event
+        buttonGoPrincipal.setOnClickListener( new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Log.i("info", "btprincipal");
+
+                obrirActivity("principal");
+            }
+        });
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.main:
+                obrirActivity("main");
+                return true;
+            case R.id.principal:
+                obrirActivity("principal");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void obrirActivity(String view) {
+
+        Log.i("info", view);
+        String classe = "";
+
+        switch (view) {
+            case "main":
+                setContentView(R.layout.activity_main);
+                Intent intentMain = new Intent(this, MainActivity.class);
+                intentMain.putExtra(EXTRA_MISSATGE, "go to main");
+                startActivity(intentMain);
+
+                break;
+            case "principal":
+                setContentView(R.layout.activity_principal);
+                Intent intentPrincipal = new Intent(this, PrincipalActivity.class);
+                intentPrincipal.putExtra(EXTRA_MISSATGE, "go to principal");
+                startActivity(intentPrincipal);
+
+                break;
+        }
+
+    }
+
+
 }
--- app/src/main/java/dam2/sopalletresandroid/PrincipalActivity.java
+++ app/src/main/java/dam2/sopalletresandroid/PrincipalActivity.java
@@ -1,13 +1,101 @@
 package dam2.sopalletresandroid;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 public class PrincipalActivity extends AppCompatActivity {
 
+    public Button buttonGoMain;
+    public Button btReset;
+    public GridView gvTauler;
+    public List<Integer> letrasMarcadas;
+    public String[] abc = new String[]{"A","B", "C", "D", "E","F", "G", "H", "I", "J","K", "L", "M", "N", "O",
+            "P", "Q", "R", "S", "T","U", "V", "W", "X", "Y", "Z"};
+    ArrayAdapter<String> Adapter;
+
+    //private TextView tvText;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        //tvText = (TextView) findViewById(R.id.tvPrincipal);
+
+
+        Intent intent = getIntent();
+        String missatge = intent.getStringExtra(MainActivity.EXTRA_MISSATGE); // get data inside intent last view
+        Log.i("info", missatge);
+        //tauler
+        gvTauler = (GridView) findViewById(R.id.gvTauler);
+        final List<Integer> letrasMarcadas = new ArrayList<>();
+        btReset = (Button) findViewById(R.id.btReset);
+        buttonGoMain = (Button) findViewById(R.id.btGoMain);
+        buttonGoMain.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.i("info", "generating...");
+                generateGridTauler();
+            }
+        });
+
+        btReset.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.i("info", "reset");
+
+                for(Integer item : letrasMarcadas){
+
+
+                }
+
+            }
+        });
+
+        gvTauler.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("info", parent.toString()+ view+ position+id  );
+                Log.i("info", view.toString()  );
+                Log.i("info", String.valueOf(position));
+                Log.i("info", String.valueOf(id ) );
+
+                System.out.println( abc[position]);
+                letrasMarcadas.add(position);
+
+                view.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
+            }
+        });
     }
+
+
+    public void generateGridTauler() {
+        buttonGoMain.setVisibility(View.GONE); // hide button generate
+
+        Adapter = new ArrayAdapter<String>(this,android.R.layout.simple_selectable_list_item, abc);
+        gvTauler.setAdapter(Adapter);
+
+
+    }
+
+
 }
